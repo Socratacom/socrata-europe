2f0aef5cdcdb4a92fe53e9d2be6002017a71db29
Michael Church <michael@fishinglounge.com>
2015-05-04 11:46:37 -0400

    progress dev

--- wp-content/themes/sage/homepage.php
+++ wp-content/themes/sage/homepage.php
@@ -5,21 +5,29 @@
 ?>
 <?php while (have_posts()) : the_post(); ?>
 <section class="homepage-hero">
-	<div class="wrapper clearfix">
-		<h1 class="text-center">Socrata makes sharing government data easier, faster and more affordable.</h1>
-		<div class="col-sm-6 hidden-xs">
-			<article class="text-center">
-				<h2>Open Data</h2>
-				<p>A scalable cloud platform helps you create a sustainable open data program from day one.</p>
-				<div class="btn-wrapper"><a href="/open-data" class="btn btn-success">Explore Open Data</a></div>
-			</article>
-		</div>
-		<div class="col-sm-6 hidden-xs">
-			<article class="text-center">
-				<h2>Open Performance</h2>
-				<p>Socrata Open Performance is a dynamic performance management and analytics application designed exclusively for the government sector.</p>
-				<div class="btn-wrapper"><a href="/open-performance" class="btn btn-success">Explore Open Performance</a></div>
-			</article>
+	<div class="vertical-align">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<h1 class="text-center">Socrata makes sharing government data easier, faster and more affordable.</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-6 col-md-5  col-md-offset-1 hidden-xs">
+					<article class="text-center">
+						<h2>Open Data</h2>
+						<p>A scalable cloud platform helps you create a sustainable open data program from day one.</p>
+						<div class="btn-wrapper"><a href="/open-data" class="btn btn-success">Explore Open Data</a></div>
+					</article>
+				</div>
+				<div class="col-sm-6 col-md-5 hidden-xs">
+					<article class="text-center">
+						<h2>Open Performance</h2>
+						<p>Socrata Open Performance is a dynamic performance management and analytics application designed exclusively for the government sector.</p>
+						<div class="btn-wrapper"><a href="/open-performance" class="btn btn-success">Explore Open Performance</a></div>
+					</article>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="text-center down-arrow"><i class="fa fa-chevron-down"></i></div>
--- wp-content/themes/sage/templates/content-newsletter.php
+++ wp-content/themes/sage/templates/content-newsletter.php
@@ -1,7 +1,16 @@
-<section class="belize-hole">
+<section class="midnight-blue newsletter">
 	<div class="container">
-
-		<p class="text-center">Newsletter Form here.</p>
-
+		<div class="row">
+			<div class="col-sm-5 col-sm-offset-1">
+				<h2>Stay In Touch</h2>
+				<h3>Subscribe to the Scorata newsletter</h3>
+				<p><strong>Transform</strong>, Socrata’s Newsletter, brings you regular updates on everything new and important in the world of open data, transparent government, and more.</p>
+			</div>
+			<div class="col-sm-5">
+				<script src="//app-abk.marketo.com/js/forms2/js/forms2.min.js"></script>
+				<form id="mktoForm_2340"></form>
+				<script>MktoForms2.loadForm("//app-abk.marketo.com", "851-SII-641", 2340);</script>
+			</div>
+		</div>
 	</div>
 </section>
