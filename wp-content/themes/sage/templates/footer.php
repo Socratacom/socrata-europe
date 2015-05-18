5541384141181513e1ce0e7044d786ecd46346dc
Michael Church <michael@fishinglounge.com>
2015-05-18 13:41:53 -0400

    progress dev

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -2,6 +2,6 @@
   <div class="container text-center">
   	<div class="small-logo"></div>
   	<p>Copyright &copy; <?php echo date("Y");?> Socrata, Inc. All rights reserved.</p>
-  	<p><a href="http://socrata.com">Socrata USA</a></p>
+  	<p><a href="http://socrata.com">Go to Socrata USA</a></p>
   </div>
 </footer>
