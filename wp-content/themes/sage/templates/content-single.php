692a29629faac0f19d5471b379cfc08c4195008e
mike-church <mike.church@fishinglounge.com>
2015-08-02 22:04:13 -0400

    progress dev

--- wp-content/themes/sage/templates/content-single.php
+++ wp-content/themes/sage/templates/content-single.php
@@ -1,9 +1,11 @@
 <?php while (have_posts()) : the_post(); ?>
-<div class="blog-hero" style="background-image: url(<?php echo blog_feature_image('full', 1600, 300); ?>); height:300px; width:100%; background-repeat: no-repeat; background-size: cover; background-position: center center;"></div>
+<div class="blog-hero" style="background-image: url(<?php echo blog_feature_image('full', 1600, 300); ?>); ">
+    <div class="hero-overlay"></div>
+</div>
 
 <div class="container">
 	<div class="row">
-		<div class="col-sm-7 col-sm-offset-1">
+		<div class="col-sm-7 col-sm-offset-1 blog-content">
 			<article <?php post_class(); ?>>
 			<header>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
@@ -12,7 +14,6 @@
 			<div class="entry-content">
 			<?php the_content(); ?>
 			</div>
-			<!--<?php comments_template('/templates/comments.php'); ?>-->
 			</article>
 		</div>
 		<div class="col-sm-3">
--- wp-content/themes/sage/templates/entry-meta.php
+++ wp-content/themes/sage/templates/entry-meta.php
@@ -1,2 +1,5 @@
-<time class="updated" datetime="<?= get_the_time('c'); ?>"><?= get_the_date(); ?></time>
-<p class="byline author vcard"><?= __('By', 'sage'); ?> <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?= get_the_author(); ?></a></p>
+<ul class="byline">
+    <li><?php echo get_avatar( get_the_author_meta('ID'), 50 ); ?></li>
+    <li>By <?php the_author(); ?></li>
+    <li>Posted: <?php the_time('F jS, Y') ?></li>
+</ul>
